78335c0b0318dfa8dcf61c15b5d47d0d0ab5d154
Orlando La <ceanela@users.noreply.github.com>
2017-08-21 10:06:06 -0500

    Update <App /> routes, Add Progress, CreateAccount and forms

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import {
   Header,
   Footer,
@@ -16,10 +16,13 @@ import {
   Users } from '../routes';
 import Modal from 'react-modal';
 import Login from './Login';
+import CreateAccount from './CreateAccount';
 
 const Scene = ({
   children,
   className = 'main-content',
+  location,
+  computedMatch,
   ...props }) => (
   <div
     className={className}
@@ -32,66 +35,101 @@ const Scene = ({
 class App extends Component {
   static defaultProps = {
     className: 'app container-fluid p-0 row no-gutters d-flex',
+    modalRoutes: [
+      '/create-account',
+      '/login',
+    ],
+  }
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.previousLocation = {
+      pathname: '/',
+      hash: '',
+      search: '',
+    };
+  }
+
+  componentWillUpdate(nextProps) {
+    const { location } = this.props;
+    // set previousLocation if props.location is not modal
+    if (
+      nextProps.history.action !== 'POP' &&
+      (!location.state || !location.state.modal)
+    ) {
+      this.previousLocation = this.props.location;
+    }
   }
 
   render() {
-    const { className, isOpen } = this.props;
+    const { className, location, modalRoutes } = this.props;
+    const isModal = modalRoutes.some(r => new RegExp(r).test(location.pathname));
+
+    console.log(this.previousLocation, location);
 
     return (
       <main className={className}>
         <Header />
         <div className="flex-fill">
           <Navbar />
-          <Scene>
-            <Route
-              component={Transfer}
-              exact
-              path="/"
-            />
-            <Route
-              component={Transfer}
-              path="/transfer"
-            />
-            <Route
-              component={TransactionHistory}
-              path="/transactions"
-            />
-            <Route
-              component={Permissions}
-              path="/permissions"
-            />
-            <Route
-              component={Profile}
-              path="/user/:id"
-            />
-            <Route
-              component={About}
-              path="/about"
-            />
-            <Route
-              component={Faqs}
-              path="/faqs"
-            />
-            <Route
-              component={Users}
-              path="/users"
-            />
-            <Route
-              component={Preferences}
-              path="/preferences"
-            />
-            <Route
-              component={NoMatch}
-              path="*"
-            />
-            <Footer />
-          </Scene>
+          <Switch location={isModal ? this.previousLocation : location}>
+            <Scene>
+              <Route
+                component={Transfer}
+                exact
+                path="/(|transfer)/"
+              />
+              <Route
+                component={TransactionHistory}
+                path="/transactions"
+              />
+              <Route
+                component={Permissions}
+                path="/permissions"
+              />
+              <Route
+                component={Profile}
+                path="/user/:id"
+              />
+              <Route
+                component={About}
+                path="/about"
+              />
+              <Route
+                component={Faqs}
+                path="/faqs"
+              />
+              <Route
+                component={Users}
+                path="/users"
+              />
+              <Route
+                component={Preferences}
+                path="/preferences"
+              />
+              <Route
+                component={NoMatch}
+              />
+
+              <Footer />
+            </Scene>
+          </Switch>
         </div>
-        <Modal
-          isOpen
-        >
-          <Login />
-        </Modal>
+        {isModal ?
+          <Modal
+            isOpen
+            contentLabel={location.pathname}
+          >
+            <Route
+              component={Login}
+              path="/login"
+            />
+            <Route
+              component={CreateAccount}
+              path="/create-account"
+            />
+          </Modal> : null}
       </main>
     );
   }
