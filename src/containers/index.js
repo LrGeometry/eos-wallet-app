04352bfded323b58c918fdda0959c5cbd80e1a34
Orlando La <ceanela@users.noreply.github.com>
2017-08-23 10:31:38 -0500

    Delete <SearchResults />, update <Balance /> name

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,6 @@
 export { default as App } from './App';
-export { default as BalanceContainer } from './BalanceContainer';
+export { default as Balance } from './Balance';
 export { default as Keystore } from './Keystore';
-export { default as SearchResults } from './SearchResults';
 export { default as Transactions } from './Transactions';
 export { default as User } from './User';
 export { default as Users } from './Users';
