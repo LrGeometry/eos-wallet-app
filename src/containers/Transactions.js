9a8fab5f3061ce4ba4314fff0036d1b569fb8db3
stubelshe <stu.belshe@tandem.ly>
2017-08-25 16:29:44 -0500

    Changing spacing

--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -17,7 +17,7 @@ const Transaction = ({ date, sender, memo, amount }) => (
       />
 
       <div className="transaction-data ml-3">
-        <p className="transaction-sender mb-0"><a>{sender.name}</a></p>
+        <p className="transaction-sender mb-1"><a>{sender.name}</a></p>
         <p className="transaction-memo mb-0">{memo}</p>
       </div>
     </div>
