41191160ac0fec8ec4ac6246fd8f6938782fc2b5
Orlando La <ceanela@users.noreply.github.com>
2017-08-28 13:31:04 -0500

    Converting to function component, logic flow from upwards

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { User } from '../containers';
 import { Link } from 'react-router-dom';
 
+const Header = ({
+  className = 'header row no-gutters items-center p-0 p-md-4',
+  handleOpen,
+  isOpen,
+}) => (
+  <header className={className}>
+    <div
+      className={`mobile-nav mr-2 hidden-md-up ${isOpen ? 'open' : 'closed'}`}
+      onClick={handleOpen}
+    >
+      <span className="icon-eos_icons_menu" />
+    </div>
+    <Link to="/">
+      <img
+        alt=""
+        className="logo"
+        src="images/logo.svg"
+      />
+    </Link>
 
-class Header extends Component {
-  static defaultProps = {
-    className: 'header row no-gutters items-center p-0 p-md-4',
-  }
-
-  constructor(props, context) {
-    super(props, context);
-    
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  navOpen(e) {
-    e.preventDefault();
-    this.setState({ isOpen: !this.state.isOpen });
-  }
-
-  render() {
-    const { className, styles } = this.props;
-    const { isOpen } = this.state;
-    const navOpen = this.navOpen.bind(this);
-
-    return (
-      <header className={className}>
-        <div className={`mobile-nav mr-2 hidden-md-up ${isOpen ? 'open' : 'closed'}`} onClick={navOpen}>
-          <span className="icon-eos_icons_menu" />
-        </div>
-        <Link to="/">
-          <img
-            alt=""
-            className="logo"
-            src="images/logo.svg"
-          />
-        </Link>
-
-        <User />
-      </header>
-    );
-  }
-}
+    <User />
+  </header>
+);
 
 export default Header;
