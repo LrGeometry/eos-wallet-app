6f6def029cc8b94d15a92c0ef47cecb035b09976
Orlando La <ceanela@users.noreply.github.com>
2017-08-28 11:26:37 -0500

    Add redux-form reducer in top level

--- src/containers/reducers.js
+++ src/containers/reducers.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
 import { reducer as app } from './App/reducer';
+
 
 const reducers = combineReducers({
   app,
+  form: reduxFormReducer,
 });
 
 export default reducers;
