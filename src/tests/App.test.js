06c984a9414bddea6cd310bc8946daff032465fe
Orlando La <ceanela@users.noreply.github.com>
2017-08-28 12:53:53 -0500

    Fix test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { StaticRouter, Route } from 'react-router';
-import App from '../containers/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App, reducers } from '../containers';
 import createHistory from 'history/createBrowserHistory';
 
 const history = createHistory();
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk),
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,7 +19,9 @@ it('renders without crashing', () => {
 
   ReactDOM.render(
     <StaticRouter location="/" context={staticContext}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StaticRouter>,
     div,
   );
