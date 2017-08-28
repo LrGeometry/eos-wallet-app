c632095c1992badeaa16150ded850503ce3d770c
stubelshe <stu.belshe@tandem.ly>
2017-08-28 13:11:48 -0500

    Testing nav structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import { User } from '../containers';
 import { Link } from 'react-router-dom';
 
+
 class Header extends Component {
   static defaultProps = {
     className: 'header row no-gutters items-center p-0 p-md-4',
   }
 
+  constructor(props, context) {
+    super(props, context);
+    
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  navOpen(e) {
+    e.preventDefault();
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
     const { className, styles } = this.props;
+    const { isOpen } = this.state;
+    const navOpen = this.navOpen.bind(this);
 
     return (
       <header className={className}>
-        <div className="mobile-nav mr-2 hidden-md-up">
+        <div className={`mobile-nav mr-2 hidden-md-up ${isOpen ? 'open' : 'closed'}`} onClick={navOpen}>
           <span className="icon-eos_icons_menu" />
         </div>
         <Link to="/">
