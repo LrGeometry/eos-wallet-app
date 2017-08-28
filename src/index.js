1749a4cfa8a1f6dd6b888d1a43256c42cf1d075f
Orlando La <ceanela@users.noreply.github.com>
2017-08-28 15:11:49 -0500

    Clean up Transactions organization, abstract middleware to its own repo

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import { App, reducers } from './containers';
+import middlewares from './middleware';
 import registerServiceWorker from './func/registerServiceWorker';
 import './styles/index.scss';
 
 const history = createHistory();
 const store = createStore(
   reducers,
-  applyMiddleware(thunk),
+  middlewares,
 );
 
 ReactDOM.render(
