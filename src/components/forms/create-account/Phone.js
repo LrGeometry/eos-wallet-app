40e87ea49d070c881d4369ec57b4391a834245f1
Orlando La <ceanela@users.noreply.github.com>
2017-08-28 10:30:03 -0500

    Add redux-form and blank reducers

--- src/components/forms/create-account/Phone.js
+++ src/components/forms/create-account/Phone.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const Phone = () => (
-  <form>
+const Phone = ({ handleSubmit, submitting }) => (
+  <form onSubmit={handleSubmit}>
     <h2>Next your phone</h2>
     <p>Enter your phone below.</p>
 
@@ -10,7 +11,7 @@ const Phone = () => (
       <div clasName="input-container">
         <div className="icon" />
         <div className="input-prefix">
-          <input
+          <Field
             aria-describedby="phone"
             className="form-control form-control-lg prefix"
             id="phone"
@@ -24,7 +25,11 @@ const Phone = () => (
     <div className="modal-cta">
       <div className="row col-12 no-gutters p-0">
         <div className="col-sm-auto col-12 pl-0 pr-sm-2 pr-0 mb-sm-0 mb-3">
-          <button className="btn btn-primary btn-lg btn-block" type="submit">
+          <button
+            disabled={submitting}
+            className="btn btn-primary btn-lg btn-block"
+            type="submit"
+          >
             Continue
           </button>
         </div>
@@ -33,4 +38,6 @@ const Phone = () => (
   </form>
 );
 
-export default Phone;
+export default reduxForm({
+  form: 'phone',
+})(Phone);
