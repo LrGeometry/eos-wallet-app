57013af18fde1d13eeb668970aebd7faa5df9291
Orlando La <ceanela@users.noreply.github.com>
2017-08-30 10:47:44 -0500

    Pass close logic down to modals

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -118,7 +118,7 @@ class App extends Component {
             contentLabel={location.pathname}
             onRequestClose={handleModalClose}
           >
-            {renderRoutes(modalRoutes)}
+            {renderRoutes(modalRoutes, { handleModalClose })}
           </Modal>}
       </main>
     );
